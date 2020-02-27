7e6be189afbf5543b744e79b2e05f4593b22c3eb
Edi Murwanto <uchihaedhi27@gmail.com>
2020-02-27 20:06:40 +0700

    [DOC] able to use swagger ui documentations

--- src/main/java/com/enigmacamp/config/DaoSpringConfig.java
+++ src/main/java/com/enigmacamp/config/DaoSpringConfig.java
@@ -3,14 +3,30 @@ package com.enigmacamp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.enigmacamp.dao.AlbumDao;
 import com.enigmacamp.dao.SingerDao;
-import com.enigmacamp.daoImpl.SingerDaoImpl;
+import com.enigmacamp.dao.SongDao;
+//import com.enigmacamp.daoImpl.SingerDaoImpl;
+import com.enigmacamp.daoImpl.db.AlbumDaoDBImpl;
+import com.enigmacamp.daoImpl.db.SingerDaoDBImpl;
+import com.enigmacamp.daoImpl.db.SongDaoDBImpl;
 
 @Configuration
 public class DaoSpringConfig {
 
 	@Bean
 	public SingerDao singerDao() {
-		return new SingerDaoImpl();
+		return new SingerDaoDBImpl();
 	}
+
+	@Bean
+	public AlbumDao albumDao() {
+		return new AlbumDaoDBImpl();
+	}
+	
+	@Bean
+	public SongDao songDao() {
+		return new SongDaoDBImpl();
+	}
+
 }
---
+++ src/main/java/com/enigmacamp/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.enigmacamp.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configurable
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.enigmacamp.controllers"))
+				.paths(PathSelectors.ant("/api/v1")).build().apiInfo(getApiInfo());
+
+	}
+	private ApiInfo getApiInfo() {
+		return new ApiInfoBuilder().title("Music-API Documentations")
+				.description("Documentations for Music-App REST-API").version("0.0.1")
+				.licenseUrl("http://localhost:8081/music-app")
+				.contact(new Contact("edhi.uchiha", "edhi.uchiha.com", "edhi.uchiha@gmail.com"))
+				.license("License of API").build();
+	}
+
+}
