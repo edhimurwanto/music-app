73a42ed1cf3eb5428e1ff0d92befd96b03d7b420
Edi Murwanto <uchihaedhi27@gmail.com>
2020-02-27 20:10:16 +0700

    [DOC] able to use swagger ui documentations

--- src/main/java/com/enigmacamp/App.java
+++ src/main/java/com/enigmacamp/App.java
@@ -3,7 +3,10 @@ package com.enigmacamp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class App {
 
 	public static void main(String[] args) {
